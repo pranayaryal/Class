4c079890f501da51d1bbb9282006fb90fc7f606c
speedoheck <drpranayaryal@gmail.com>
2014-12-10 00:03:12 -0500

    replaced by the code copied from github gist which shows dynamic UI elements

--- shinyApp/server.R
+++ shinyApp/server.R
@@ -1,113 +1,54 @@
-
 library(shiny)
+
 shinyServer(function(input, output) {
   
-  output$samplesize <- renderPrint({
-    z=1-(input$alpha)/2
-    qz=qnorm(z,0,1)
-    qb=qnorm((1-input$beta),0,1)
-    ((qz+qb)^2)*((input$sd)^2)/((input$improv)^2)
-  })
-  output$tstat <- renderPrint({
-    ts=(input$change)/((input$sd1)/sqrt(input$sample))
-    ts
+  output$ui <- renderUI({
+    if (is.null(input$input_type))
+      return()
+    
+    # Depending on input$input_type, we'll generate a different
+    # UI component and send it to the client.
+    switch(input$input_type,
+           "slider" = sliderInput("dynamic", "Dynamic",
+                                  min = 1, max = 20, value = 10),
+           "text" = textInput("dynamic", "Dynamic",
+                              value = "starting value"),
+           "numeric" =  numericInput("dynamic", "Dynamic",
+                                     value = 12),
+           "checkbox" = checkboxInput("dynamic", "Dynamic",
+                                      value = TRUE),
+           "checkboxGroup" = checkboxGroupInput("dynamic", "Dynamic",
+                                                choices = c("Option 1" = "option1",
+                                                            "Option 2" = "option2"),
+                                                selected = "option2"
+           ),
+           "radioButtons" = radioButtons("dynamic", "Dynamic",
+                                         choices = c("Option 1" = "option1",
+                                                     "Option 2" = "option2"),
+                                         selected = "option2"
+           ),
+           "selectInput" = selectInput("dynamic", "Dynamic",
+                                       choices = c("Option 1" = "option1",
+                                                   "Option 2" = "option2"),
+                                       selected = "option2"
+           ),
+           "selectInput (multi)" = selectInput("dynamic", "Dynamic",
+                                               choices = c("Option 1" = "option1",
+                                                           "Option 2" = "option2"),
+                                               selected = c("option1", "option2"),
+                                               multiple = TRUE
+           ),
+           "date" = dateInput("dynamic", "Dynamic"),
+           "daterange" = dateRangeInput("dynamic", "Dynamic")
+    )
   })
   
-  output$decision <-renderPrint({
-    tstat=(input$change)/((input$sd1)/sqrt(input$sample))
-    pval=pt(tstat,(input$sample-1))
-    
-    t=qt((1-input$alpha1),(input$sample-1))
-    ## converting radiobutton input to numeric value
-    sw=as.numeric(input$ckg)
-    
-    if (sw==1){
-      ##t=qt((1-input$alpha1),(input$sample-1))
-      if (tstat > t){
-        cat("Null Hypothesis Rejected with a p value of ",pval)
-        
-        }
-      else {
-        cat("Null Hypothesis Accepted with a p value of ",pval)
-        
-        }
-       
-      }
-     
-     else if (sw==2){
-           if (tstat < t){
-         cat("Null Hypothesis Rejected with a p value of ",pval)
-        
-          }
-            else {
-        cat("Null Hypothesis Accepted with a p value of ",pval)
-            
-              }
-         
-         }
-     
-     else if (sw==3){
-          t=qt((1-(input$alpha1)/2),(input$sample-1))
-            if (abs(tstat) > t){
-              cat("Null Hypothesis Rejected with a p value of ",pval)
-              
-                }
-          else {
-            cat("Null Hypothesis Accepted with a p value of ",pval)
-            
-                }
-            
-            }
-    })
+  output$input_type_text <- renderText({
+    input$input_type
+  })
   
-  output$decisionmean <-renderPrint({
-    tstat=(input$meansample)/((input$sdmean)/sqrt(input$meansample))
-    pval=pt(tstat,(input$meansample-1))
-    
-    t=qt((1-input$meanalpha),(input$meansample-1))
-    ## converting radiobutton input to numeric value
-    sw=as.numeric(input$ckgmean)
-    
-    if (sw==1){
-      ##t=qt((1-input$alpha1),(input$sample-1))
-      if (tstat > t){
-        cat("Null Hypothesis Rejected with a p value of ",pval)
-        
-      }
-      else {
-        cat("Null Hypothesis Accepted with a p value of ",pval)
-        
-      }
-      
-    }
-    
-    else if (sw==2){
-      if (tstat < t){
-        cat("Null Hypothesis Rejected with a p value of ",pval)
-        
-      }
-      else {
-        cat("Null Hypothesis Accepted with a p value of ",pval)
-        
-      }
-      
-    }
-    
-    else if (sw==3){
-      t=qt((1-(input$meanalpha)/2),(input$meansample-1))
-      if (abs(tstat) > t){
-        cat("Null Hypothesis Rejected with a p value of ",pval)
-        
-      }
-      else {
-        cat("Null Hypothesis Accepted with a p value of ",pval)
-        
-      }
-      
-    }
-    
-    
+  output$dynamic_value <- renderPrint({
+    str(input$dynamic)
   })
-    
-    
-  })
+  
+})
--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -1,111 +1,29 @@
 
 library(shiny)
 shinyUI(fluidPage(
-  titlePanel("Pranay Aryal's Biostatistics App"),
-  withMathJax(),
-  
+  titlePanel("Dynamically generated user interface components"),
   fluidRow(
     
-    column(2,
-           wellPanel(
-             div("PAIRED SAMPLE TEST OF MEANS",style ="color:blue"),
-             hr(),
-             numericInput("improv", label = h4("Enter improvement desired"), value = NULL),
-             hr(),
-             ##p("Current Value:", style = "color:#888888;"), 
-             ##verbatimTextOutput("num"),
-             numericInput("beta",label=h4("enter beta"),value=NULL),
-             hr(),
-             ##p("Current Value:", style = "color:#888888;"), 
-             ##verbatimTextOutput("num2"),
-             numericInput("alpha",label=h4("enter alpha"),value=NULL),
-             sliderInput("sd","Enter standard deviation",min=0,max=100,value=10,step=1),
-             p("sample size",style ="color:#888888;")
-             
-           )),
+    column(3, wellPanel(
+      selectInput("input_type", "Input type",
+                  c("slider", "text", "numeric", "checkbox",
+                    "checkboxGroup", "radioButtons", "selectInput",
+                    "selectInput (multi)", "date", "daterange"
+                  )
+      )
+    )),
+    
+    column(3, wellPanel(
+      # This outputs the dynamic UI component
+      uiOutput("ui")
+    )),
+    
     column(3,
-           wellPanel(
-             div("PAIRED SAMPLE TEST OF MEANS(TEST STATISTICS)",style ="color:blue"),
-             hr(),
-             withMathJax(),
-             radioButtons("ckg", 
-                label = h4("Null and Alternate Hypothesis"), 
-                choices = list("\\(H_0:\\mu_d \\le \\mu_{d0}
-                    ;H_A:\\mu_d > \\mu_{d0}\\)" = 1,
-                "\\(H_0:\\mu_d \\ge \\mu_{d0};H_A:\\mu_d < \\mu_{d0}\\)" = 2, 
-                "\\(H_0:\\mu_d = \\mu_{d0};H_A:\\mu_d \\ne \\mu_{d0}\\)" = 3),
-                selected = 1),
-             sliderInput("sample", "Enter sample size",min=0,max=100,value=50),
-             hr(),
-             sliderInput("change","enter desired average change",min=-10,max=20,
-                         step=0.1,value=-3.2),
-             hr(),
-             sliderInput("sd1","enter standard deviation",min=0,
-                         max=30,step=0.1,value=12.1),
-             numericInput("alpha1","enter alpha",value=0.05),
-             p("t-statistics",style ="color:#888888;"),
-             verbatimTextOutput("tstat"),
-             p("decision",style ="color:#888888;"),
-             textOutput("decision")
-           )),
-    column(3,
-           wellPanel(
-             div("ONE SAMPLE TEST OF MEAN",style ="color:blue"),
-             hr(),
-             withMathJax(),
-             radioButtons("ckgmean", 
-                  label = h4("Null and Alternate Hypothesis"), 
-                  choices = list("\\(H_0:\\mu \\le \\mu_0;H_A:\\mu > \\mu_0\\)" = 1,
-                 "\\(H_0:\\mu \\ge \\mu_0;H_A:\\mu < \\mu_0\\)" = 2, 
-                 "\\(H_0:\\mu = \\mu_0;H_A:\\mu \\ne \\mu_0\\)" = 3),
-                          selected = 1),
-             sliderInput("meansize", "Enter sample size",min=0,max=100,value=50),
-             hr(),
-             numericInput("nullmean","null value",value=-50),
-             hr(),
-             numericInput("meansample","sample mean",value=-50),
-             sliderInput("sdmean","enter standard deviation",
-                         min=0,max=30,step=0.1,value=12.1),
-             numericInput("meanalpha","enter alpha",value=0.05),
-             p("t-statistics",style ="color:#888888;"),
-             verbatimTextOutput("tstat"),
-             p("decision",style ="color:#888888;"),
-             textOutput("decisionmean")
-           )),
-    column(4,
-           wellPanel(
-             div("TWO SAMPLE TEST OF DIFFERENCE IN MEAN(DIFFERENT VARIANCE)",
-                 style ="color:blue"),
-             hr(),
-             withMathJax(),
-             radioButtons("ckg", 
-                    label = h4("Null and Alternate Hypothesis"), 
-                    choices = list("\\(H_0:\\mu_1-\\mu_2 \\le (\\mu_1-\\mu_2)_0\\;
-                      H_A:\\mu_1-\\mu_2 > (\\mu_1-\\mu_2)_0\\)" = 1,
-                         "\\(H_0:\\mu_1-\\mu_2 \\ge (\\mu_1-\\mu_2)_0;
-                         H_A:\\mu_1-\\mu_2 < (\\mu_1-\\mu_2)_0\\)" = 2, 
-                       "\\(H_0:\\mu_1-\\mu_2 = (\\mu_1-\\mu_2)_0\\;
-                        H_A:\\mu_1-\\mu_2 \\ne (\\mu_1-\\mu_2)_0\\)" = 3),                 
-                        selected = 1),
-             selectInput("select", strong("Select variance"), 
-                         choices = list("Equal Variance" = 1, 
-                          "Unequal Variance" = 2),
-                         selected = NULL),
-             uiOutput("ui"),
-             numericInput("meandiffsample", "Enter sample size",value=50),
-             hr(),
-             numericInput("nullmeandiff","null value",value=-50),
-             hr(),
-             numericInput("meandiff","difference of mean",value=-50),
-             numericInput("sdmeandiff1","enter standard deviation1",
-                         value=NULL),
-             numericInput("sdmeandiff2","enter standard deviation2",
-                          value=NULL),
-             numericInput("meandiffalpha","enter alpha",value=0.05),
-             p("t-statistics",style ="color:#888888;"),
-             verbatimTextOutput("tstat"),
-             p("decision",style ="color:#888888;"),
-             textOutput("decisionmeandiff")
-           ))
+           tags$p("Input type:"),
+           verbatimTextOutput("input_type_text"),
+           tags$p("Dynamic input value:"),
+           verbatimTextOutput("dynamic_value")
     )
+  )
 ))
+  
